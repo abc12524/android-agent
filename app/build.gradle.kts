f9e684f76f204a4889eb248ade74cba6f4e63d77
hollowd <hollowd@qq.com>
2026-09-04 15:14:09 +0800

    fix: 添加 kotlin.plugin.compose 插件，移除已废弃的 composeOptions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
 }
 
@@ -53,10 +54,6 @@ android {
     buildFeatures {
         compose = true
         buildConfig = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "2.0.21"
     }
 }
 
